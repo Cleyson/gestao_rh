89e4b3a6b8b753f11e71c478fb40dd2deef17538
Cleyson <cleyson.bonfim@gmail.com>
2023-06-05 17:18:08 -0300

    iniciando crud hora-extra

--- apps/documentos/urls.py
+++ apps/documentos/urls.py
@@ -3,6 +3,6 @@ from .views import DocumentoCreate
 
 
 urlpatterns = [
-    path('novo', DocumentoCreate.as_view(), name='create_documento'),
+    path('novo/<int:funcionario_id>/', DocumentoCreate.as_view(), name='create_documento'),
    # path('delete/<int:pk>/', DocumentoEdit.as_view(), name='edit_Documento'),
 ]
